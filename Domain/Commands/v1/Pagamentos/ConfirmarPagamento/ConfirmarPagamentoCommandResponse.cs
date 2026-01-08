bd546ae86e54596d062e26e76d7114c73683af3f
Vitor Renno <vitorrenno@outlook.com>
2026-01-08 18:31:17 -0300

    Incluindo confirmarpagamento no MapperProfiles.

--- Domain/Commands/v1/Pagamentos/ConfirmarPagamento/ConfirmarPagamentoCommandResponse.cs
+++ Domain/Commands/v1/Pagamentos/ConfirmarPagamento/ConfirmarPagamentoCommandResponse.cs
@@ -1,15 +1,9 @@
 ﻿using Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Domain.Commands.v1.Pagamentos.ConfirmarPagamento
+namespace Domain.Commands.v1.Pagamentos.ConfirmarPagamento;
+
+public class ConfirmarPagamentoCommandResponse
 {
-    public class ConfirmarPagamentoCommandResponse
-    {
-        public Guid Id { get; set; }
-        public StatusPagamento Status { get; set; }
-    }
+    public Guid Id { get; set; }
+    public StatusPagamento Status { get; set; }
 }
